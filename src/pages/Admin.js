bfd8dc492eb661aa3b1c037521799e7e43c6770b
vuongcdt <vuongcdt@gmail.com>
2022-10-12 13:05:31 +0700

    checkout

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -4,16 +4,18 @@ import React, { useEffect } from "react";
 function Admin() {
    useEffect(() => {
       const token = localStorage.getItem("token");
-      axios.get(`${process.env.REACT_APP_DATABASE_HOST}/admin`, { headers: { Authorization: `Bearer ${token}` } }).then((res) => {
+      // axios.get(`${process.env.REACT_APP_DATABASE_HOST}/admin`, { headers: { Authorization: `Bearer ${token}` } }).then((res) => {
+      axios.get(`http://localhost:5001/api/admin`, { headers: { Authorization: `Bearer ${token}` } }).then((res) => {
          const elm = document.createElement("script");
          elm.type = "text/javascript";
-         elm.id = "java";
+         elm.id = "javascript";
          elm.innerHTML = res.data;
          document.head.append(elm);
       });
+      // return document.getElementById("javascript").innerHTML=''
    }, []);
 
-   return <div id="admin"></div>;
+   return <div id="admin" className="mt-5"></div>;
 }
 
 export default Admin;
