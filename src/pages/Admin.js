9a99b44684d2ab694395d37a93e619de8aba2bf9
vuongcdt <vuongcdt@gmail.com>
2022-10-12 13:10:09 +0700

    fix admin back to home.

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -4,8 +4,8 @@ import React, { useEffect } from "react";
 function Admin() {
    useEffect(() => {
       const token = localStorage.getItem("token");
-      // axios.get(`${process.env.REACT_APP_DATABASE_HOST}/admin`, { headers: { Authorization: `Bearer ${token}` } }).then((res) => {
-      axios.get(`http://localhost:5001/api/admin`, { headers: { Authorization: `Bearer ${token}` } }).then((res) => {
+      axios.get(`${process.env.REACT_APP_DATABASE_HOST}/admin`, { headers: { Authorization: `Bearer ${token}` } }).then((res) => {
+      // axios.get(`http://localhost:5001/api/admin`, { headers: { Authorization: `Bearer ${token}` } }).then((res) => {
          const elm = document.createElement("script");
          elm.type = "text/javascript";
          elm.id = "javascript";
