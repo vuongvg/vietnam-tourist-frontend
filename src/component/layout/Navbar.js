eda78b63b2730cb1e4becafa137d0e2b9fcf519c
vuongcdt <vuongcdt@gmail.com>
2022-10-21 15:24:05 +0700

    Revert "update menu mobile"
    This reverts commit 7d827dc4ab960275d6dfae732df362eac9d6f724.

--- src/component/layout/Navbar.js
+++ src/component/layout/Navbar.js
@@ -11,7 +11,6 @@ function Navbar() {
    const [backGroundColor, setBackGroundColor] = useState("");
    const [currentUser, setCurrentUser] = useState(null);
    const [scrollY, setScrollY] = useState();
-   const [showHideMenuMobile, setShowHideMenuMobile] = useState(false);
 
    function setBackground() {
       if (urlPath[1] && (urlPath[1] === "detail" || urlPath[1] === "search")) {
@@ -55,10 +54,6 @@ function Navbar() {
       localStorage.removeItem("userRole");
    };
    const userRole = localStorage.getItem("userRole");
-
-   const handleShowHideMobileMenu = () => {
-      setShowHideMenuMobile(!showHideMenuMobile);
-   }
 
    return (
       <>
@@ -107,64 +102,8 @@ function Navbar() {
          </div>
          <div className="d-block d-sm-none position-fixed vw-100 z-1" style={{ backgroundColor: "#222831", transition: "1s" }}>
             <div className="text-white d-flex justify-content-between py-3 px-4">
-               <div>
-                  <Link to="/">
-                     <h3 className="text-white mb-0">VNTOUR</h3>
-                  </Link>
-               </div>
-               <MenuIcon onClick={}/>
-            </div>
-            <div hidden={!showHideMenuMobile} className="text-center">
-               <div className="py-2">
-                  {userRole === "admin" && (
-                     <NavLink className="menu-item" to="/admin">
-                        <div className="d-inline-flex align-items-center">
-                           <ManageAccounts />
-                           <div className="ms-1">Admin</div>
-                        </div>
-                     </NavLink>
-                  )}
-               </div>
-               <div className="py-2">
-                  <NavLink className="menu-item" id="home" to="/">
-                     Home
-                  </NavLink>
-               </div>
-               <div className="py-2">
-                  <NavLink className="menu-item" to="/hotel">
-                     Hotel
-                  </NavLink>
-               </div>
-               <div className="py-2">
-                  <NavLink className="menu-item" to="/restaurant">
-                     Restaurant
-                  </NavLink>
-               </div>
-               <div className="py-2">
-                  <NavLink className="menu-item" to="/tour">
-                     Tour
-                  </NavLink>
-               </div>
-               <div className="py-2">
-                  <NavLink className="menu-item" to="/blog">
-                     Blog
-                  </NavLink>
-               </div>
-               <div className="py-2">
-                  <NavLink className="menu-item" to="/about">
-                     About
-                  </NavLink>
-               </div>
-               <div className="py-2">
-                  <NavLink className="menu-item" to="/contact">
-                     Contact
-                  </NavLink>
-               </div>
-               <div className="py-2">
-                  <NavLink className="menu-item" to="/login">
-                     {currentUser ? <Logout onClick={handleLogout} /> : <AccountCircle />}
-                  </NavLink>
-               </div>
+               <div>dirEngine</div>
+               <MenuIcon />
             </div>
          </div>
       </>
