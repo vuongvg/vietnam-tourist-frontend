b3f075d48e30eb97f1e1d8063b717dcdfa1c6963
DoMinhHieu1997 <hieudm112@gmail.com>
2022-10-25 21:33:02 +0700

    update detail page

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,11 +1,14 @@
 import DetailContent from "../component/Detail/DetailContent";
 import SideBar from "../component/Detail/SideBar";
+import SideBarBlog from "../component/DetailBlog/SideBar";
 import { useLocation } from "react-router-dom";
+import { Tour } from "@mui/icons-material";
 
 function Detail () {
 
   const { pathname } = useLocation();
-   const urlPath = pathname.split("/");
+  const urlPath = pathname.split("/");
+  console.log(urlPath[2]);
 
   return (
     <div className="container mb-5" style={{paddingTop:'10rem'}}>
@@ -13,9 +16,15 @@ function Detail () {
         <div className="col-12 col-md-9">
           <DetailContent detailType={urlPath[2]} idPost={urlPath[3]}/>
         </div>
-        <div className="col-12 col-md-3">
-          <SideBar detailType={urlPath[2]} idPost={urlPath[3]}/>
-        </div>
+        {
+          urlPath[2] !== "hotel" && urlPath[2] !== "hotel"
+            ?
+              <SideBarBlog/>
+            :
+              <div className="col-12 col-md-3">
+                <SideBar detailType={urlPath[2]} idPost={urlPath[3]}/>
+              </div>
+        }
       </div>
     </div>
   )
